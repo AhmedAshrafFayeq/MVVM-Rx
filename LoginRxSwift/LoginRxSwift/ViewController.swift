832e85f3ef1e93c7850e0ec6ee126516dc11553c
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-04-30 20:47:46 +0200

    configure UI visual elements

--- LoginRxSwift/LoginRxSwift/ViewController.swift
+++ LoginRxSwift/LoginRxSwift/ViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    @IBAction func didTapLoginButton(_ sender: Any) {
+        
+    }
 }
 
