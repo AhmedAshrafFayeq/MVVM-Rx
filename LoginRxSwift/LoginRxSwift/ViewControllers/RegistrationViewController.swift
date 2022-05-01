870000f5df36014589322a8b4874a4555a1ae54d
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-01 15:24:42 +0200

    add RegisterButton

---
+++ LoginRxSwift/LoginRxSwift/ViewControllers/RegistrationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegistrationViewController.swift
+//  LoginRxSwift
+//
+//  Created by Ahmed Fayeq on 01/05/2022.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func didTapRegisterButton(_ sender: Any) {
+        
+    }
+}
