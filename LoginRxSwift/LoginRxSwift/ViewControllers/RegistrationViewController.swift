9046b4ec682cf96af815cab4922e9c0b01087ce1
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-01 16:03:33 +0200

    bind Register visual elements to ViewModel

--- LoginRxSwift/LoginRxSwift/ViewControllers/RegistrationViewController.swift
+++ LoginRxSwift/LoginRxSwift/ViewControllers/RegistrationViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import RxSwift
 
 class RegistrationViewController: UIViewController {
-
+    
+    let viewModel   = RegisterViewModel()
+    let disposeBag  = DisposeBag()
+    
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -16,10 +20,15 @@ class RegistrationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        firstNameTextField.rx.text.map{ $0 ?? "" }.bind(to: viewModel.firstNamePublishSubject).disposed(by: disposeBag)
+        lastNameTextField.rx.text.map{ $0 ?? "" }.bind(to: viewModel.lastNamePublishSubject).disposed(by: disposeBag)
+        emailTextField.rx.text.map{ $0 ?? "" }.bind(to: viewModel.emailPublishSubject).disposed(by: disposeBag)
+        
+        viewModel.isValid().bind(to: registerButton.rx.isEnabled).disposed(by: disposeBag)
+        viewModel.isValid().map{ $0 ? 1 : 0.1 }.bind(to: registerButton.rx.alpha).disposed(by: disposeBag)
     }
 
     @IBAction func didTapRegisterButton(_ sender: Any) {
-        
     }
 }
