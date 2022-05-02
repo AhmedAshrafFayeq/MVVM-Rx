1cd800b9fdabda325440754d10f56d8f2435a217
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-02 18:15:16 +0200

    add HomeVC

--- LoginRxSwift/LoginRxSwift/ViewControllers/HomeViewController.swift
+++ LoginRxSwift/LoginRxSwift/ViewControllers/HomeViewController.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    let userDefaults = UserDefaults.standard
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Home"
+        nameLabel.text = "Welcome \(userDefaults.string(forKey: "name") ?? "")"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
