5ef42f806938fcc1ed830b4688424b234196dfb3
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-08 16:57:28 +0200

    add some observable and subscribes

--- RxSwift-main/Rx.playground/Pages/Demo.xcplaygroundpage/Contents.swift
+++ RxSwift-main/Rx.playground/Pages/Demo.xcplaygroundpage/Contents.swift
@@ -2,4 +2,20 @@ import RxSwift
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-let numberSequence = 
+let numberSequence = Observable.just(5)
+let numberSubscriptiopn = numberSequence.subscribe(onNext: {
+    print($0)
+})
+
+let helloSequence = Observable.from(["H","E","L","L","O"])
+
+let helloSubscription = helloSequence.subscribe { event in
+    switch event {
+    case .next(let value):
+        print(value)
+    case .error(let error):
+        print(error)
+    case .completed:
+        print("Completed")
+    }
+}
