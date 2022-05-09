589a0a079829af0dd0ce4a61c6305e2eb6e14ee0
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-09 19:45:54 +0200

    add disposeBag

--- RxSwift-main/Rx.playground/Pages/Demo.xcplaygroundpage/Contents.swift
+++ RxSwift-main/Rx.playground/Pages/Demo.xcplaygroundpage/Contents.swift
@@ -2,10 +2,12 @@ import RxSwift
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+let disposeBag = DisposeBag()
+
 let numberSequence = Observable.just(5)
 let numberSubscriptiopn = numberSequence.subscribe(onNext: {
     print($0)
-})
+}).disposed(by: disposeBag)
 
 let helloSequence = Observable.from(["H","E","L","L","O"])
 
@@ -18,4 +20,4 @@ let helloSubscription = helloSequence.subscribe { event in
     case .completed:
         print("Completed")
     }
-}
+}.disposed(by: disposeBag)
