53eae76eb2d09b6433f74955ef73615970484dd1
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-10 17:34:52 +0200

    add first challenge

--- RxSwift-main/Rx.playground/Pages/Observables - Challenge.xcplaygroundpage/Contents.swift
+++ RxSwift-main/Rx.playground/Pages/Observables - Challenge.xcplaygroundpage/Contents.swift
@@ -1,1 +1,11 @@
 
+import RxSwift
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let disposeBag = DisposeBag()
+let numberSequence = Observable.from([1,2,3,4,5,6,7,8])
+let numberSupscription = numberSequence.subscribe (onNext: {
+    var number = $0 * 5
+    print(number)
+}).disposed(by: disposeBag)
