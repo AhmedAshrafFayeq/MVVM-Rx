544ce3f9ae5e7beb5e4ac0818d5d195a097d83cf
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-10 17:36:27 +0200

    enhance code

--- RxSwift-main/Rx.playground/Pages/Observables - Challenge.xcplaygroundpage/Contents.swift
+++ RxSwift-main/Rx.playground/Pages/Observables - Challenge.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 let disposeBag = DisposeBag()
 let numberSequence = Observable.from([1,2,3,4,5,6,7,8])
-let numberSupscription = numberSequence.subscribe (onNext: {
-    var number = $0 * 5
+let numberSupscription: () = numberSequence.subscribe (onNext: {
+    let number = $0 * 5
     print(number)
 }).disposed(by: disposeBag)
--- RxSwift-main/Rx.playground/Pages/Observables.xcplaygroundpage/Contents.swift
+++ RxSwift-main/Rx.playground/Pages/Observables.xcplaygroundpage/Contents.swift
@@ -5,13 +5,13 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 let disposeBag = DisposeBag()
 
 let numberSequence = Observable.just(5)
-let numberSubscriptiopn = numberSequence.subscribe(onNext: {
+let numberSubscriptiopn: () = numberSequence.subscribe(onNext: {
     print($0)
 }).disposed(by: disposeBag)
 
 let helloSequence = Observable.from(["H","E","L","L","O"])
 
-let helloSubscription = helloSequence.subscribe { event in
+let helloSubscription: () = helloSequence.subscribe { event in
     switch event {
     case .next(let value):
         print(value)
